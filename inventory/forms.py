39dc59f318786c4dc36f63b3bc7f1cccf88c97ff
Ezra Reich <ezra@reich.be>
2022-10-06 18:24:31 +0100

    Tailoring NewRecipeRequirement: __init__

--- inventory/forms.py
+++ inventory/forms.py
@@ -11,7 +11,29 @@ class MenuItemForm(forms.ModelForm):
         model = MenuItem
         fields = '__all__'
 
+
+def get_choice(lst):
+        choices = []
+        for i in range(len(lst)):
+            temp_lst = lst[i][0]
+            temp_tuple = (temp_lst, temp_lst)
+            choices.append(temp_tuple)
+        
+        return choices
+    
+   
 class RecipeRequirementForm(forms.ModelForm):
+    '''
+   def __init__(self, *args, **kwargs):
+        
+        super(RecipeRequirementForm, self).__init__(*args, **kwargs)
+       
+        titles = get_choice([ (o.title, o) for o in MenuItem.objects.all()])
+        self.fields['menu_item'] = forms.ChoiceField(choices=titles)
+        
+        names = get_choice([ (o.name, o) for o in Ingredient.objects.all()])
+        self.fields['ingredient'] = forms.ChoiceField(choices=names)'''
+
     class Meta:
         model = RecipeRequirement
         fields = '__all__'
